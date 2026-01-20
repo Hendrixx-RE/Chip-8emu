a3ad84a1986eb52b11f9e006b104946520156eab
Hendrixx-RE <athukuttan2004@gmail.com>
2026-01-21 00:58:48 +0530

    final commit this time

---
+++ build/CMakeFiles/Chip8.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chip8.dir/link.d"
+  "CMakeFiles/Chip8.dir/src/Cpu.cpp.o"
+  "CMakeFiles/Chip8.dir/src/Cpu.cpp.o.d"
+  "CMakeFiles/Chip8.dir/src/inputOutput.cpp.o"
+  "CMakeFiles/Chip8.dir/src/inputOutput.cpp.o.d"
+  "CMakeFiles/Chip8.dir/src/main.cpp.o"
+  "CMakeFiles/Chip8.dir/src/main.cpp.o.d"
+  "Chip8"
+  "Chip8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chip8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
